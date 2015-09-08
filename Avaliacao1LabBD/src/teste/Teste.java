eba641fdee39d412b0e71073e1cbeed414aabe33
Pedro <Pedro@Pedro-PC>
2015-09-08 18:53:09 -0300

    TESTE

---
+++ Avaliacao1LabBD/src/teste/Teste.java
@@ -0,0 +1,11 @@
+package teste;
+
+import javax.swing.JOptionPane;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "SE É LOCO CACHUERA");
+	}
+
+}
