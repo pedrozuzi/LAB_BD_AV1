63a2690b3d17a5ba6df6a065c0387d33ccde6504
Pedro <Pedro@Pedro-PC>
2015-10-15 16:14:24 -0300

    janela grupos (inacabada)

--- Avaliacao1LabBD/src/boundary/FrmGrupos.java
+++ Avaliacao1LabBD/src/boundary/FrmGrupos.java
@@ -1,5 +1,48 @@
 package boundary;
 
-public class FrmGrupos {
+import java.awt.Color;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.LineBorder;
+
+public class FrmGrupos {
+	
+	private JFrame janela;
+	private JPanel panPrincipal;
+	private JPanel panel;
+	private JTable grupoA;
+	private JTable grupoB;
+	private JTable grupoC;
+	private JTable grupoD;
+	private JScrollPane scrollPane;
+	
+	public FrmGrupos() {
+		janela = new JFrame();
+		panPrincipal = new JPanel();
+		panel = new JPanel();
+		
+        janela.setLocationRelativeTo(null);
+        janela.setResizable(false);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.setVisible(true);
+		
+		grupoA = new JTable();
+		//grupoA.addMouseListener(this);
+		grupoA.setBorder(new LineBorder(Color.BLACK));
+		grupoA.setGridColor(Color.BLACK);
+		grupoA.setShowGrid(true);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.getViewport().setBorder(null);
+		scrollPane.setViewportView(grupoA);
+		scrollPane.setBounds(100, 50, 717, 167);
+		panPrincipal.add(scrollPane);
+		
+		janela.setSize(749,677);
+		janela.setContentPane( panPrincipal );
+	}
+	
 }
