5a0291a33042eaf8181356ead0ff781c1ce8ff79
BlueTub <hury_gabriel@hotmail.com>
2015-10-25 11:53:56 -0200

    up

--- Avaliacao1LabBD/src/boundary/FrmGruposResultados.java
+++ Avaliacao1LabBD/src/boundary/FrmGruposResultados.java
@@ -26,7 +26,7 @@ import control.CtrlResultados;
 import entity.GruposResultados;
 
 public class FrmGruposResultados {
-	
+
 	private JFrame janela;
 	private JPanel panPrincipal;
 	private JScrollPane scrollPaneA;
@@ -48,140 +48,142 @@ public class FrmGruposResultados {
 	private JMenuBar menuBarra;
 	private JMenu menu;
 	private JMenuItem menuPrincipal;
-	
+
 	public FrmGruposResultados() {
 		controleGrupos = new CtrlGrupos();
 		janela = new JFrame("Grupos Paulistão");
 		panPrincipal = new JPanel();
 		panPrincipal.setBackground(Color.WHITE);
-		
+
 		menuBarra = new JMenuBar();
 		janela.setJMenuBar(menuBarra);
-		
+
 		menu = new JMenu("Menu");
 		menuBarra.add(menu);
-		
+
 		menuPrincipal = new JMenuItem("Menu Principal");
-		menuPrincipal.setIcon(new ImageIcon(getClass()
-				.getResource("/img/HomeMenu.png")));
+		menuPrincipal.setIcon(new ImageIcon(getClass().getResource("/img/HomeMenu.png")));
 		menu.add(menuPrincipal);
-		
 
 		scrollPane = new JScrollPane();
 		scrollPane.setBounds(0, 0, 805, 583);
 		panPrincipal.add(scrollPane);
-		
+
 		panel_1 = new JPanel();
 		scrollPane.setViewportView(panel_1);
 		panel_1.setLayout(null);
-		
+
 		grupoA = new JTable() {
-			
+
 			@Override
-			public Component prepareRenderer(TableCellRenderer renderer,
-					int row, int column) {
+			public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
 				Component c = super.prepareRenderer(renderer, row, column);
 				System.out.println((grupoA.getValueAt(column, 0).toString().toLowerCase()));
 				if (grupoA.getValueAt(row, 0).toString().toLowerCase().equalsIgnoreCase("Ituano")) {
 					System.out.println("IF");
-					c.setBackground(new Color(192,0,0));
+					c.setBackground(new Color(192, 0, 0));
 					c.setForeground(Color.WHITE);
-				}else{
+				} else {
 					c.setForeground(getForeground());
 					System.out.println("ELSE");
 				}
 				return c;
 			}
 		};
-		
+
 		grupoA.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		grupoA.setBorder(new LineBorder(Color.BLACK));
 		grupoA.setGridColor(Color.BLACK);
 		grupoA.setShowGrid(true);
-		
+
 		scrollPaneA = new JScrollPane();
 		scrollPaneA.setBounds(10, 40, 785, 103);
 		panel_1.add(scrollPaneA);
 		scrollPaneA.getViewport().setBorder(null);
 		scrollPaneA.setViewportView(grupoA);
-		
+
 		grupoB = new JTable();
 		grupoB.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		grupoB.setBorder(new LineBorder(Color.BLACK));
 		grupoB.setGridColor(Color.BLACK);
 		grupoB.setShowGrid(true);
-		
+
 		scrollPaneB = new JScrollPane();
 		scrollPaneB.setBounds(10, 177, 785, 103);
 		panel_1.add(scrollPaneB);
 		scrollPaneB.getViewport().setBorder(null);
 		scrollPaneB.setViewportView(grupoB);
-		
+
 		grupoC = new JTable();
 		grupoC.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		grupoC.setBorder(new LineBorder(Color.BLACK));
 		grupoC.setGridColor(Color.BLACK);
 		grupoC.setShowGrid(true);
-		
+
 		scrollPaneC = new JScrollPane();
 		scrollPaneC.setBounds(10, 310, 785, 103);
 		panel_1.add(scrollPaneC);
 		scrollPaneC.getViewport().setBorder(null);
 		scrollPaneC.setViewportView(grupoC);
-		
+
 		grupoD = new JTable();
 		grupoD.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		grupoD.setBorder(new LineBorder(Color.BLACK));
 		grupoD.setGridColor(Color.BLACK);
 		grupoD.setShowGrid(true);
-		
+
 		scrollPaneD = new JScrollPane();
 		scrollPaneD.setBounds(10, 441, 785, 103);
 		panel_1.add(scrollPaneD);
 		scrollPaneD.getViewport().setBorder(null);
 		scrollPaneD.setViewportView(grupoD);
-		
+
 		JLabel lblGrupoB = new JLabel("Grupo B");
 		lblGrupoB.setFont(new Font("Tahoma", Font.BOLD, 18));
 		lblGrupoB.setBounds(366, 143, 100, 33);
 		panel_1.add(lblGrupoB);
-		
+
 		JLabel lblGrupoC = new JLabel("Grupo C");
 		lblGrupoC.setFont(new Font("Tahoma", Font.BOLD, 18));
 		lblGrupoC.setBounds(366, 279, 100, 33);
 		panel_1.add(lblGrupoC);
-		
+
 		JLabel lblGrupoD = new JLabel("Grupo D");
 		lblGrupoD.setFont(new Font("Tahoma", Font.BOLD, 18));
 		lblGrupoD.setBounds(366, 411, 100, 33);
 		panel_1.add(lblGrupoD);
-		
+
 		lblGrupoA = new JLabel("Grupo A");
 		lblGrupoA.setBounds(366, 11, 100, 33);
 		panel_1.add(lblGrupoA);
 		lblGrupoA.setFont(new Font("Tahoma", Font.BOLD, 18));
-		
-		carregaTabelas();
-			
-//			DefaultTableCellRenderer linha = new DefaultTableCellRenderer();
-//			linha.setForeground(Color.WHITE);
-//			linha.setBackground(Color.RED);
-//			grupoA.getColumnModel().getColumn(0).setCellRenderer(linha);
-		
 
-			
+		carregaTabelas();
+
+		// DefaultTableCellRenderer linha = new DefaultTableCellRenderer();
+		// linha.setForeground(Color.WHITE);
+		// linha.setBackground(Color.RED);
+		// grupoA.getColumnModel().getColumn(0).setCellRenderer(linha);
+
+		// //pinta as linhas dos rebaixados de vermelho
+		// CtrlResultados ctrl = new CtrlResultados();
+		// grupoA = ctrl.rebaixados(5, grupoA);
+		// grupoB = ctrl.rebaixados(5, grupoB);
+		// grupoC = ctrl.rebaixados(5, grupoC);
+		// grupoD = ctrl.rebaixados(5, grupoD);
+
 		menuPrincipal.addActionListener(l -> {
 			janela.dispose();
 			janela = null;
 			new FrmPrincipal();
 		});
-		
-		janela.setSize(811,602);
-		janela.setContentPane( panPrincipal );
+
+		janela.setSize(811, 602);
+		janela.setContentPane(panPrincipal);
 		panPrincipal.setLayout(null);
-		
+
 		janela.setLocationRelativeTo(null);
-        janela.setResizable(false);
+		janela.setResizable(false);
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		janela.setVisible(true);
 	}
@@ -191,43 +193,31 @@ public class FrmGruposResultados {
 		List<GruposResultados> listaGrupoB = new ArrayList<GruposResultados>();
 		List<GruposResultados> listaGrupoC = new ArrayList<GruposResultados>();
 		List<GruposResultados> listaGrupoD = new ArrayList<GruposResultados>();
-		
+
 		listaGrupoA = controleGrupos.buscaGruposResultados("A");
 		listaGrupoB = controleGrupos.buscaGruposResultados("B");
-		listaGrupoC= controleGrupos.buscaGruposResultados("C");
+		listaGrupoC = controleGrupos.buscaGruposResultados("C");
 		listaGrupoD = controleGrupos.buscaGruposResultados("D");
-		
-		
-		
-		if ( !listaGrupoA.isEmpty() &&
-				!listaGrupoB.isEmpty() &&
-				!listaGrupoC.isEmpty() &&
-				!listaGrupoD.isEmpty()) {
-			
+
+		if (!listaGrupoA.isEmpty() && !listaGrupoB.isEmpty() && !listaGrupoC.isEmpty() && !listaGrupoD.isEmpty()) {
+
 			modeloA = new ModeloTabela(listaGrupoA);
 			modeloB = new ModeloTabela(listaGrupoB);
 			modeloC = new ModeloTabela(listaGrupoC);
 			modeloD = new ModeloTabela(listaGrupoD);
-			
+
 			grupoA.getTableHeader().setReorderingAllowed(false);
 			grupoA.setModel(modeloA);
-			
+
 			grupoB.getTableHeader().setReorderingAllowed(false);
 			grupoB.setModel(modeloB);
-			
+
 			grupoC.getTableHeader().setReorderingAllowed(false);
 			grupoC.setModel(modeloC);
-			
+
 			grupoD.getTableHeader().setReorderingAllowed(false);
 			grupoD.setModel(modeloD);
-			
-			//pinta as linhas
-			CtrlResultados ctrl = new CtrlResultados();
-			grupoA = ctrl.rebaixados(5, grupoA);
-			grupoB = ctrl.rebaixados(5, grupoB);
-			grupoC = ctrl.rebaixados(5, grupoC);
-			grupoD = ctrl.rebaixados(5, grupoD);
-			
+
 		}
 	}
 }
